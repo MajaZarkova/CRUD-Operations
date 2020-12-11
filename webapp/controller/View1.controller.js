4d942a2588a0ad03ec22c062c964928ec8e07255
MajaZarkova <majazarkova@gmail.com>
2020-12-11 12:24:58 +0000

    Refactored

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,10 +8,7 @@ sap.ui.define([
 		onInit: function () {},
 
 		oDataCall: function (oEvent) {
-			// call oData service's function based on which button is clicked.
-			debugger;
 			var myModel = this.getView().getModel();
-			// CREATE******************
 			if ("Create" == oEvent.oSource.mProperties.text) {
 				var obj = {};
 				obj.Id = this.getView().byId("uniqueid").getValue();
@@ -20,58 +17,14 @@ sap.ui.define([
 				obj.Mobile = this.getView().byId("mobid").getValue();
 				myModel.create("/userdataSet", obj, {
 					success: function (oData, oResponse) {
-						debugger;
 						alert("Record Created Successfully...");
 					},
 					error: function (err, oResponse) {
-						debugger;
 						alert("Error while creating record - "
 							.concat(err.response.statusText));
 					}
 				});
 			}
-			// READ******************
-			else if ("Read" == oEvent.oSource.mProperties.text) {
-				var readurl = "/userdataSet('" + this.getView().byId("uniqueid").getValue() + "')";
-				var tab = this.getView().byId("userdatatable");
-				myModel.read(readurl, {
-					success: function (oData, oResponse) {
-						debugger;
-						var userdata = new sap.ui.model.json.JSONModel({
-							"Result": oData.results
-						});
-
-						tab.setModel(userdata);
-						var i = 0;
-						tab.bindAggregation("items", {
-							path: "userdata>/Result",
-							template: new sap.m.ColumnListItem({
-								cells: [new sap.ui.commons.TextView({
-									text: "{userdata>Id}",
-									design: "H5",
-									semanticColor: "Default"
-								}), new sap.ui.commons.TextView({
-									text: "{userdata>Name}",
-									design: "H5",
-									semanticColor: "Positive"
-								}), new sap.ui.commons.TextView({
-									text: "{userdata>Email}",
-									design: "H5",
-									semanticColor: "Positive"
-								}), new sap.ui.commons.TextView({
-									text: "{userdata>Mobile}",
-									design: "H5",
-									semanticColor: "Positive"
-								}) ]
-							})
-						});
-					},
-					error: function (err) {
-						debugger;
-					}
-				});
-			}
-			// UPDATE******************
 			if ("Update" == oEvent.oSource.mProperties.text) {
 				var updateobj = {};
 				updateobj.Id = this.getView().byId("uniqueid").getValue();
@@ -82,26 +35,21 @@ sap.ui.define([
 
 				myModel.update(updateurl, updateobj, {
 					success: function (oData, oResponse) {
-						debugger;
 						alert("Record Updated Successfully...");
 					},
 					error: function (err, oResponse) {
-						debugger;
 						alert("Error while updating record - "
 							.concat(err.response.statusText));
 					}
 				});
 			}
-			// DELETE******************
 			if ("Delete" == oEvent.oSource.mProperties.text) {
 				var delurl = "/userdataSet('" + this.getView().byId("uniqueid").getValue() + "')";
 				myModel.remove(delurl, {
 					success: function (oData, oResponse) {
-						debugger;
 						alert("Record Removed Successfully...");
 					},
 					error: function (err, oResponse) {
-						debugger;
 						alert("Error while removing record - "
 							.concat(err.response.statusText));
 					}
